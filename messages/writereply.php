f9da83b7981cb152b701d0c58152364bd013f5e0
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-06-30 19:00:08 +0000

    Removed extraneous echo output

--- messages/writereply.php
+++ messages/writereply.php
@@ -3,8 +3,6 @@ include_once('lib/common.php');
 $isAllowed = true;
 
 if ($isAllowed) {
-  echo 'allowed';
-
   $hmsg->addMessage($_POST['msg_to'],$_POST['msg_from'],$_POST['msg_about'],$_POST['type'],$_POST['msg_purpose'],$_POST['consent'],$_POST['belief'],$_POST['message'],
   $_POST['consent_required'], $_POST['msg_id']);
   echo '<a href="viewmsg.php">view messages</a>';
