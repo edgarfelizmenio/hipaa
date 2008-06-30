ebcdbe1ceecc4eb76d1f98c12c9cab2726d4789b
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-06-30 23:07:37 +0000

    Added dojo toolkit

--- messages/tpl/header.php
+++ messages/tpl/header.php
@@ -1,5 +1,13 @@
+    <script type="text/javascript" src="http://www.stanford.edu/~stevetan/cgi-bin/hipaa/messages/lib/dojo-release-1.1.1/dojo/dojo.js"
+	    djConfig="parseOnLoad: true">
+    </script>
+    <style type="text/css">
 
-<script type="text/javascript" src="http://www.stanford.edu/~stevetan/cgi-bin/prolog/messages/lib/js/unobtrusivedynamicselect_ex5.js"></script>
-<script type="text/javascript" src="http://www.stanford.edu/~stevetan/cgi-bin/prolog/messages/lib/js/usableforms.js"></script>
-<link rel="stylesheet"
-href="http://www.stanford.edu/~stevetan/cgi-bin/prolog/messages/tpl/form.css" />
+      @import "http://www.stanford.edu/~stevetan/cgi-bin/hipaa/messages/lib/dojo-release-1.1.1/dijit/themes/tundra/tundra.css";
+      @import "http://www.stanford.edu/~stevetan/cgi-bin/hipaa/messages/lib/dojo-release-1.1.1/dojo/resources/dojo.css";
+    </style>
+    <script type="text/javascript" src="http://www.stanford.edu/~stevetan/cgi-bin/hipaa/messages/lib/js/enforcer.js">
+
+<script type="text/javascript" src="http://www.stanford.edu/~stevetan/cgi-bin/hipaa/messages/lib/js/unobtrusivedynamicselect_ex5.js"></script>
+<script type="text/javascript" src="http://www.stanford.edu/~stevetan/cgi-bin/hipaa/messages/lib/js/usableforms.js"></script>
+<link rel="stylesheet" href="http://www.stanford.edu/~stevetan/cgi-bin/hipaa/messages/tpl/form.css" />
