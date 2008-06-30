3d6a61e83c61a847df356c91996ec8e15f0bb540
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-06-30 23:07:08 +0000

    Added dojo to the file and move some parts of form into tpl

--- messages/write.php
+++ messages/write.php
@@ -2,37 +2,43 @@
   <head>
     <?php include('tpl/header.php'); ?>
   </head>
-  <body>
+  <body id="body" class="tundra">
     <?php include('tpl/msg_menu.php'); ?>
     <h2>Write Message</h2>
     <form method="post" action="writemsg.php">
       <div>
 	<label>From:</label>
-	<select name="msg_from">
-          <option VALUE="">-- Select whom you are -- </option>
-	  <?php
-	     include ('tpl/recipients.php');
 
-	     ?>
+	<select id="msg_from"
+		name="msg_from"
+		dojoType="dijit.form.FilteringSelect"
+		onChange="fieldChanged('msg_from');"
+		autocomplete="false">
+<?php	     include ('tpl/recipients.php');	     ?>
 	</select>
 
       </div>
 
       <div>
 	<label>To:</label>
-	<select name="msg_to">
-          <option VALUE="">-- Select the recipient -- </option>
-	  <?php
-	     include ('tpl/recipients.php');
-
-	     ?>
+	<select name="msg_to"
+		id="msg_to"
+		dojoType="dijit.form.FilteringSelect"
+		autocomplete="false"
+		onChange="fieldChanged('msg_to');"
+	    name="First Name">
+	  <?php	     include ('tpl/recipients.php');	     ?>
 	</select>
 
       </div>
 
       <div>
 	<label>About:</label>
-	<select name="msg_about">
+	<select name="msg_about"
+		id="msg_about"
+		dojoType="dijit.form.FilteringSelect"
+		autocomplete="false"
+		onChange="fieldChanged('msg_about');">
 	  <?php
 	     include ('tpl/about.php');
 	     ?>
@@ -46,7 +52,11 @@
 
       <div>
 	<label>Purpose:</label>
-	<select name="msg_purpose">
+	<select id="msg_purpose"
+		name="msg_purpose"
+		dojoType="dijit.form.FilteringSelect"
+		autocomplete="false"
+		onChange="fieldChanged('msg_about');">
 	  <?php
 	     include ('tpl/purpose.php');
 	     ?>
@@ -58,35 +68,9 @@
 	    <input type="text" name="replyto" />
 	    </div>
 	-->
-      <div>
-	<label>Belief:</label>
-	<input type="checkbox" name="belief" value="1" />
-      </div>
-
-
-      <div>
-	<label>Message:</label>
-	<textarea name="message"></textarea>
-      </div>
-
-      <div>
-	<label>Requires Consent?</label>
-	<input name="consent_required" value="true" type="checkbox" rel="permission">
-
-      </div>
-
-      <div rel="permission">
-	<label>Consented by:</label>
-		<select name="consent">
-          <option VALUE="">-- Choose consent -- </option>
-	  <?php
-	     include ('tpl/recipients.php');
-
-	     ?>
-	</select>
-
-      </div>
-
+<?php
+   include ('tpl/msg_footer.php');
+?>
 
 
       <div>
