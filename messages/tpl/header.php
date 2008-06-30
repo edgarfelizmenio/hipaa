9445ec7a74e80aa9132b1fbf7f73c91f542d46b8
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-06-30 23:26:31 +0000

    Changed paths from absolute to relative

--- messages/tpl/header.php
+++ messages/tpl/header.php
@@ -3,11 +3,11 @@
     </script>
     <style type="text/css">
 
-      @import "http://www.stanford.edu/~stevetan/cgi-bin/hipaa/messages/lib/dojo-release-1.1.1/dijit/themes/tundra/tundra.css";
-      @import "http://www.stanford.edu/~stevetan/cgi-bin/hipaa/messages/lib/dojo-release-1.1.1/dojo/resources/dojo.css";
+      @import "lib/dojo-release-1.1.1/dijit/themes/tundra/tundra.css";
+      @import "lib/dojo-release-1.1.1/dojo/resources/dojo.css";
     </style>
-    <script type="text/javascript" src="http://www.stanford.edu/~stevetan/cgi-bin/hipaa/messages/lib/js/enforcer.js">
+    <script type="text/javascript" src="lib/js/enforcer.js">
 
-<script type="text/javascript" src="http://www.stanford.edu/~stevetan/cgi-bin/hipaa/messages/lib/js/unobtrusivedynamicselect_ex5.js"></script>
-<script type="text/javascript" src="http://www.stanford.edu/~stevetan/cgi-bin/hipaa/messages/lib/js/usableforms.js"></script>
-<link rel="stylesheet" href="http://www.stanford.edu/~stevetan/cgi-bin/hipaa/messages/tpl/form.css" />
+<script type="text/javascript" src="lib/js/unobtrusivedynamicselect_ex5.js"></script>
+<script type="text/javascript" src="lib/js/usableforms.js"></script>
+<link rel="stylesheet" href="tpl/form.css" />
