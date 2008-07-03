00645e767a8960ea29fc0d4e9cbebcdf39a4e244
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-07-03 21:47:16 +0000

    Added style for invalid options

--- messages/tpl/header.php
+++ messages/tpl/header.php
@@ -1,6 +1,9 @@
 <script type="text/javascript" src="lib/js/jquery-1.2.6.js"></script>          
    <style type="text/css">
-
+   option.invalid {
+ color: red;
+ background-color: pink;
+ }
 
    </style>
    <script type="text/javascript" src="lib/js/enforcer.js">
