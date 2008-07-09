4abc420173323dc5fc0e59eb2e6bdb18588724b0
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-07-09 23:09:51 +0000

    Changed for faster implementation

--- messages/possibilities.php
+++ messages/possibilities.php
@@ -8,11 +8,22 @@ $action = $_GET['action'];
 switch ($action) {
  case 'refresh':
    // remove the file
-   shell_exec("rm -rf lib/scripts/allpossibles");
+   $filename = 'lib/scripts/allpossibles';
+   shell_exec("rm -rf $filename");
+   shell_exec("rm -rf $filename.php");
    // generate a new one
 
    $prologCall = "setof(t(Msg_to,Msg_from,Msg_about,Msg_purpose), pbh(a(Msg_to,Msg_from,Msg_about,phi,Msg_purpose,null,null,null)),L).";        
-   shell_exec("cd lib/scripts/; ./xsbexec '" . $prologCall . "' > allpossibles");
+   shell_exec("cd lib/scripts/; ./xsbexec '" . $prologCall . "' | perl msg_possibles.pl > allpossibles");
+
+   $fh = fopen($filename, 'r');
+   $string = fread($fh, filesize($filename));
+   fclose($fh);
+   
+   $fh = fopen($filename . 'serialized.php', 'w');
+   
+   fwrite($fh, serialize(json_decode($string)));
+   fclose($fh);
 
    break;
 
@@ -33,7 +44,7 @@ switch ($action) {
 </form>
 <?php
    echo "<pre>";
-   echo shell_exec("cd lib/scripts; cat allpossibles | perl msg_possibles.pl");
+   echo shell_exec("cd lib/scripts; cat allpossibles");
    echo "</pre>";
 
 
