e370717f436385e47a5f055f8e0bfcc35c920072
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-07-09 20:08:01 +0000

    Code rehaul added process script for write forward

---
+++ messages/process/writeforward.php
@@ -0,0 +1,14 @@
+<h2>Forwarding Message</h2>
+<?php
+
+print_r($_POST);
+if ($hmsg->addMessage($_POST['consent_required'], $_POST['msg_id'])) {
+  echo "<p>Messaged added successfully</p>";
+ } else {
+  echo "<p>Not allowed</p>";
+  echo '<a href="Javascript:history.go(-1);">Go Back</a>';
+ } 
+
+
+
+?>
