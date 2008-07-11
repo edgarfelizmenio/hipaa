a9b7772ff8a67f1d27ed565a90797e82cfce8e8d
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-07-11 22:18:32 +0000

    Updated front page

--- messages/index.php
+++ messages/index.php
@@ -4,9 +4,16 @@
 <?php include('tpl/header_bot.php'); ?>
 
 <h2>Simple hospital messaging + Prolog</h2>
-
-<p>Welcome to the HIPAA Compliance Checker Demo site featuring
-  the integration of prolog into a simple hospital messaging system</p>
-
+<div class="body">
+<p>Welcome to the HIPAA Compliance Checker Demo site</p>
+<p>
+The demo shows how the HIPAA Compliance checker can be integrated into
+existing systems to enforce policies.  In this case, we have a simple hospital
+message system that follows HIPAA guidelines.   Messages are not allowed
+if they violate HIPAA in some way.  The demo also shows possible courses
+of actions based on certain situations.  Please go to the <a href='write.php'>write a message
+page</a> to view the example.
+</p>
+</div>
 <?php include('tpl/footer.php'); ?>
 
