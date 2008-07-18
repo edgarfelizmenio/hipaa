59326c45aa5f668fd94ce63c47efbe6865e34d0f
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-07-18 22:54:05 +0000

    Added belief and consent support

--- messages/tpl/msg_footer.php
+++ messages/tpl/msg_footer.php
@@ -1,20 +1,25 @@
       <div>
 	<label>Belief:</label>
-	<input type="checkbox" rel="belief_present" name="msg_belief" value="1" />
+	<input 
+	   type="checkbox" 
+	   rel="belief_present" 
+	   name="msg_belief" 
+	   value="1" 
+	   <?php if($_POST['msg_belief']=='1') echo 'checked="checked"'; ?> />
       </div>
 
 <div rel="belief_present">
 <div>
   <label>About</label>
-  <input type="text" name="b_about" />
+  <input type="text" name="belief_about" />
 </div>
 <div>
-  <label>Belief</label>
-  <input type="text" name="b_belief" />
+  <label>What Belief</label>
+  <input type="text" name="belief_what" />
 </div>
 <div>
-  <label>From</label>
-  <input type="text" name="b_from" />
+  <label>By/From</label>
+  <input type="text" name="belief_by" />
 </div>
 </div>
 
@@ -26,7 +31,12 @@
 
       <div>
 	<label>Requires Consent?</label>
-	<input name="consent_required" value="true" type="checkbox" rel="permission">
+	<input 
+	 name="consent_required" 
+	 value="1" 
+	 type="checkbox"
+	 rel="permission"
+	 <?php if($_POST['consent_required']=='1') echo 'checked="checked"'; ?>>
 
       </div>
 
@@ -35,7 +45,7 @@
 		<select name="msg_consent">
           <option VALUE="">-- Choose consent -- </option>
 	  <?php
-	     include ('tpl/recipients.php');
+	     include ('tpl/consents.php');
 
 	     ?>
 	</select>
