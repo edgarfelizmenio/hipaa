fe14fe8e666e90d631eedcae4b6b7744812d930c
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-07-28 23:51:48 +0000

    Added more variables to fill with setof and cleaned up code a bite

--- messages/possibilities.php
+++ messages/possibilities.php
@@ -1,26 +1,41 @@
+
 <html>
 <head>
 </head>
 
 <body>
 <?php
+ /**
+  * Using the prolog method setof, this script generates all the possible
+  * values that can be used in our standard HIPAA call.  It takes the
+  * ouput of the prolog call, sends it to a perl script to be parsed and
+  * saves the ouput into two formats: JSON and serialized PHP.  
+  *
+  * The serialized PHP is for other PHP scripts to be able to access the
+  * possible values in a native format.
+  */
 $action = $_GET['action'];
 switch ($action) {
  case 'refresh':
-   // remove the file
-   $filename = 'lib/scripts/allpossibles';
-   shell_exec("rm -rf $filename");
-   shell_exec("rm -rf $filename.php");
+   $filepath = 'lib/scripts/';
+   $filename = 'allpossibles';
+
+   // remove the file that currently holds old possible values
+   shell_exec("rm -rf $filename"); // JSON
+   shell_exec("rm -rf $filename.php"); // PHP serialized
+
    // generate a new one
+   $prologCall = "setof(t(Msg_to,Msg_from,Msg_about,Msg_purpose,Belief_about,Belief_what,Belief_by),pbh(a(Msg_to,Msg_from,Msg_about,phi,Msg_purpose,null,null,b(Belief_about,Belief_what,Belief_by))),L).";        
+   //$prologCall = "setof(t(Msg_to,Msg_from,Msg_about,Msg_purpose), pbh(a(Msg_to,Msg_from,Msg_about,phi,Msg_purpose,null,null,null)),L).";        
+   // perl processes into JSON
+   shell_exec("cd lib/scripts/; ./xsbexec '" . $prologCall . "' | perl  msg_possibles.pl > " . $filename);
 
-   $prologCall = "setof(t(Msg_to,Msg_from,Msg_about,Msg_purpose), pbh(a(Msg_to,Msg_from,Msg_about,phi,Msg_purpose,null,null,null)),L).";        
-   shell_exec("cd lib/scripts/; ./xsbexec '" . $prologCall . "' | perl msg_possibles.pl > allpossibles");
-
-   $fh = fopen($filename, 'r');
-   $string = fread($fh, filesize($filename));
+   $fh = fopen($filepath . $filename, 'r');
+   $string = fread($fh, filesize($filepath . $filename));
    fclose($fh);
    
-   $fh = fopen($filename . 'serialized.php', 'w');
+   // save a PHP serialized version
+   $fh = fopen($filepath . $filename . 'serialized.php', 'w');
    
    fwrite($fh, serialize(json_decode($string)));
    fclose($fh);
@@ -28,13 +43,8 @@ switch ($action) {
    break;
 
  default:
-
    break;
-  
-
  }
-
-
 
 ?>
 
