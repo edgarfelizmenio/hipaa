19b0cf8b8426dab86d5611016e664b31dd1c55f9
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-07-28 21:23:37 +0000

    Replaced with content that pulls from prolog script

--- messages/tpl/consents.php
+++ messages/tpl/consents.php
@@ -1,20 +1,16 @@
-          <option VALUE="dr_cox"<?php if ($_POST['msg_consent'] == "dr_cox") echo " selected"; ?>>Dr Cox (Doctor)</option>
-          <option VALUE="carla"<?php if ($_POST['msg_consent'] == "carla") echo " selected"; ?>>Carla (nurse)</option>
-          <option VALUE="dr_kelso"<?php if ($_POST['msg_consent'] == "dr_kelso") echo " selected"; ?>>Dr Kelso (Chief of Medicine)</option>
-          <option VALUE="dr_turk"<?php if ($_POST['msg_consent'] == "dr_turk") echo " selected"; ?>>Dr Turk (Surgeon)</option>
-          <option VALUE="dr_jd"<?php if ($_POST['msg_consent'] == "dr_jd") echo " selected"; ?>>Dr JD (Intern)</option>
-          <option VALUE="dr_elliot"<?php if ($_POST['msg_consent'] == "dr_elliot") echo " selected"; ?>>Dr Elliot (Intern)</option>
-          <option VALUE="ted"<?php if ($_POST['msg_consent'] == "ted") echo " selected"; ?>>Ted (Lawyer)</option>
-          <option VALUE="j"<?php if ($_POST['msg_consent'] == "j") echo " selected"; ?>>Janitor</option>
-          <option VALUE="lavern"<?php if ($_POST['msg_consent'] == "lavern") echo " selected"; ?>>Lavern (secretary)</option>
-          <option VALUE="jordon"<?php if ($_POST['msg_consent'] == "jordon") echo " selected"; ?>>Jordon (Board Member)</option>
-          <option VALUE="seattle_grace_hospital"<?php if ($_POST['msg_consent'] == "seattle_grace_hospital") echo " selected"; ?>>Seattle grace Hospital (Business Associate)</option>
-          <option VALUE="sacred_heart_hospital"<?php if ($_POST['msg_consent'] == "sacred_heart_hospital") echo " selected"; ?>>Sacred Heart Hospital (Covered Entity)</option>
-          <option VALUE="patient"<?php if ($_POST['msg_consent'] == "patient") echo " selected"; ?>>(patient)</option>
-          <option VALUE="thief"<?php if ($_POST['msg_consent'] == "thief") echo " selected"; ?>>(thief)</option>
-          <option VALUE="cop"<?php if ($_POST['msg_consent'] == "cop") echo " selected"; ?>>(cop)</option>
-          <option VALUE="teen"<?php if ($_POST['msg_consent'] == "teen") echo " selected"; ?>>(teenager)</option>
-          <option VALUE="kid"<?php if ($_POST['msg_consent'] == "kid") echo " selected"; ?>>(kid)</option>
-          <option VALUE="mom"<?php if ($_POST['msg_consent'] == "mom") echo " selected"; ?>>(mom)</option>
-          <option VALUE="dad"<?php if ($_POST['msg_consent'] == "dad") echo " selected"; ?>>(dad)</option>
-          <option VALUE="dead"<?php if ($_POST['msg_consent'] == "dead") echo " selected"; ?>>(dead person)</option>
+<?php
+$filename = 'lib/scripts/allpossiblesserialized.php';
+$fh = fopen($filename, 'r');
+$string = fread($fh, filesize($filename));
+$options = unserialize($string);
+
+$recipients = $options->Msg_consent->items;
+
+foreach($recipients as $recipient) {
+  $value = $recipient->name;
+  $selected = ($_POST['msg_consent'] == $value) ? 'selected="selected"' : '';
+  echo "<option value='$value' $selected> $value </option>\n";
+
+}
+
+?>
