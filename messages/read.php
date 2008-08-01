6c83675eb5150ea25c33bf3941548851024aa218
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-08-01 21:08:53 +0000

    Changed look of read.php w/ hide shows now

--- messages/read.php
+++ messages/read.php
@@ -42,35 +42,58 @@ $msg = $hmsg->getMessage($message_id);
 if ($mailbag) {
 ?>
 
-<table class="striped" border=1>
-  <tr>
-    <th>To</th>
-    <th>From</th>
-    <th>About</th>
-    <th>Type</th>
-    <th>Purpose</th>
-    <th>Message</th>
-    <th>Reply-to</th>
-  </tr>
+<div id="history">
+<button onclick="$('table.msg').show();"> Show all</button>
+<button onclick="$('table.msg').hide();"> Hide all</button>
 
 <?php
    
+
+
    foreach ($mailbag as $history) {
      $history->consent = (empty($history->consent)) ? 'N/A' : $history->consent;
 
+     echo "<dl onclick=\"\$('#msg$history->message_id').toggle()\"> <dt>Date: " . date("D, M j, g:i A", strtotime($history->date)) . "</dt><dd>From "
+     . $history->from . " to " . $history->to . ' <span>'
+     . substr($history->message,0,20) . "...<a href='#'>show/hide</a></span></dd></dl>";
+
+     echo "<table style='display:none' class='unstriped msg' id='msg" .$history->message_id . "'>";
      echo "<tr>";
+     echo "<th>To</th>";
      echo "<td>" . $history->to . "</td>";
-     echo "<td>" . $history->from . "</td>";
-     echo "<td>" . $history->about . "</td>";
-     echo "<td>" . $history->type . "</td>";
-     echo "<td>" . $history->purpose . "</td>";
-     echo "<td>" . $history->message . "</td>";
-     echo "<td><a href='reply.php?message_id=" . $msg->message_id
-     . "&replyto_id=" . $history->message_id . "'>Reply</a></td>";
      echo "</tr>";
+
+     echo "<tr>";
+     echo "<th>From</th>";
+     echo "<td>" . $history->from . "</td>";
+     echo "</tr>";
+
+     echo "<tr>";
+     echo "<th>About</th>";
+     echo "<td>" . $history->about . "</td>";
+     echo "</tr>";
+
+     echo "<tr>";
+     echo "<th>Type</th>";
+     echo "<td>" . $history->type . "</td>";
+     echo "</tr>";
+
+     echo "<tr>";
+     echo "<th>Purpose</th>";
+     echo "<td>" . $history->purpose . "</td>";
+     echo "</tr>";
+
+     echo "<tr>";
+     echo "<td colspan='2'>" . $history->message . "</td>";
+     echo "</tr>";
+
+echo "<td colspan='2'><a href='reply.php?message_id=" . $msg->message_id
+ . "&replyto_id=" . $history->message_id . "'>Reply</a></td>";
+
+     echo "</table>";
   }
   echo "</table>";
  }
 ?>
-
+</div>
 <?php include('tpl/footer.php'); ?>
