d0648d0ac673640965911c3193ddeab98873e9ee
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-08-05 22:27:21 +0000

    Shows logged in logout button now

--- messages/tpl/header_bot.php
+++ messages/tpl/header_bot.php
@@ -1,4 +1,15 @@
  </head>
  <body>
    <?php include('tpl/msg_mast.php'); ?>
+   <?php include('tpl/loginout.php'); ?>
+
+<?php 
+   if (isLoggedIn()) {
+     echo '<p>Welcome ' . $_SESSION['username'] . '. Not '
+   . $_SESSION['username'] . 
+   '? <a href="logout.php?redirect=' . basename($_SERVER['PHP_SELF']) . '">Logout</a></p>';
+   }
+   
+?>
+      
    <div id="content">
