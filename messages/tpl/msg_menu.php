66ba61f00a74678dc02f23318f6530c2dab7bc69
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-08-05 22:29:09 +0000

    Cleaned up menu

--- messages/tpl/msg_menu.php
+++ messages/tpl/msg_menu.php
@@ -2,10 +2,9 @@
   <ul id="navbar">
     <li><a href="index.php">Intro</a></li>
     <li><a href="inbox.php">Inbox</a></li>
-    <li><a href="write.php">Write Message</a></li>
-    <li><a href="viewmsg.php">View All Message</a></li>
-    <li><a href="consent.php">View Consents required</a></li>
+    <li><a href="write.php">Compose Message</a></li>
     <li><a href="viewmsg.php">Debug (See all messages)</a></li>
+    <li><a href="debugwrite.php">Debug Compose</a></li>
   </ul>
 </div>
 
