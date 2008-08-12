a08f121ed0c076157ae6e5346bf83795a95cfafa
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-08-12 18:00:35 +0000

    Removed tpl directory to be moved to inc/tpl

--- messages/tpl/loginout.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isLoggedIn()) {
-  include ('logother_form.php');
- } else {
-  include ('login_form.php');
- }
-
-?>
--- messages/tpl/msg_mast.php
+++
@@ -1,6 +0,0 @@
-
-<div id="head">
-   <h1>HIPAA Compliance Checker - Messaging Demo</h1>
-   <?php include('tpl/msg_menu.php'); ?>
-</div>
-
