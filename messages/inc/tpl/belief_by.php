7ae197f090433dce8dcd8fcbb2b047f3510e37ad
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-08-13 21:09:44 +0000

    Modified to reflect new serialized format, decomposed

--- messages/inc/tpl/belief_by.php
+++ messages/inc/tpl/belief_by.php
@@ -1,13 +1,13 @@
 <?php
-$filename = 'lib/scripts/allpossiblesserialized.php';
+$filename = CACHE_PATH . CACHE_FILE;
 $fh = fopen($filename, 'r');
 $string = fread($fh, filesize($filename));
 $options = unserialize($string);
 
-$recipients = $options->Msg_consent->items;
+$recipients = $options->Msg_consent;
 
-foreach($recipients as $recipient) {
-  $value = $recipient->name;
+for($i = 0; $i < count($recipients); $i++) {
+  $value = $recipients[$i];
   $selected = ($_POST['belief_by'] == $value) ? 'selected="selected"' : '';
   echo "<option value='$value' $selected> $value </option>\n";
 
