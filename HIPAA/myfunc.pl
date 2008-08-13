6b850d9f89635978f5c4d64025f3038bb79fd6a0
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-08-13 21:01:06 +0000

    Rename+added filter functions for better clarity and added is_role_leaf predicate

--- HIPAA/myfunc.pl
+++ HIPAA/myfunc.pl
@@ -1,28 +1,50 @@
 %:- ['odbc.pl'].
 
 
-% filter_list_any(Unfiltered, Filtered)
+% filter_var_any(Unfiltered, Filtered)
 %  filters and replaces _h2345 type vars with anything
 %  and then terminates recursion
-filter_list_any([], []).
-filter_list_any([H|T], [X|Y]) :-
+filter_var_any([], []).
+filter_var_any([H|T], [X|Y]) :-
 	atom(H),
 	H=X,
-	filter_list(T,Y).
-filter_list_any(_, [X|Y]) :-
+	filter_var_any(T,Y).
+filter_var_any(_, [X|Y]) :-
         X='anything',
 	Y = [].
 
 
-% filter_list(Unfiltered, Filtered)
+% filter_vars(Unfiltered, Filtered)
 %  filters the list, skipping over _h234 type variables
-filter_list([], []).
-filter_list([H|T], [X|Y]) :-
+filter_vars([], []).
+filter_vars([H|T], [X|Y]) :-
 	atom(H),
 	H=X,
-	filter_list(T,Y).
-filter_list([_|T], L) :-
-	filter_list(T, L).
+	filter_vars(T,Y).
+filter_vars([_|T], L) :-
+	filter_vars(T, L).
+
+
+% filter_inner_role_nodes(RoleList, LeafRoles)
+% 
+%   Filters out the inner nodes of the role tree, leaving only leaf roles
+%   of the predicate inRole.  Eg. doctor, nurse, surgeon are filtered out,
+%   leaving dr_cox, carla, dr_turk 
+filter_inner_role_nodes([], []).
+filter_inner_role_nodes([H|T], [X|Y]) :-
+	atom(H),
+        is_role_leaf(H),
+	H=X,
+	filter_inner_role_nodes(T,Y).
+filter_inner_role_nodes([_|T], L) :-
+	filter_inner_role_nodes(T, L).
+
+
+% is_role_leaf(Role)
+%   Success if the role given is a leaf node
+%   In other words, noone else is inRole of this Role.
+is_role_leaf(Role) :- 
+	not(inRole(_,Role)). /* Node grounded */
 
 
 
