019a4c2bb6af4d47b6e7a7858f2e04facc4a92f2
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-08-13 21:15:35 +0000

    Added more constants and comments for clarity

--- messages/common.php
+++ messages/common.php
@@ -1,35 +1,36 @@
 <?php
-define('ROOTPATH', dirname(__FILE__) . '/');
-define('INC_PATH', ROOTPATH . 'inc/');
-define('EZSQL_PATH', INC_PATH . 'ezsql/');
-define('TPL_PATH' , INC_PATH . 'tpl/');
-define('CACHE_PATH', INC_PATH . 'cache/');
+define('ROOTPATH', dirname(__FILE__) . '/'); // /var/www/messages/
+define('INC_PATH', ROOTPATH . 'inc/'); // /var/www/messages/inc/
+define('EZSQL_PATH', INC_PATH . 'ezsql/'); // /var/www/messages/inc/ezsql/
+define('TPL_PATH' , INC_PATH . 'tpl/'); // /var/www/messages/inc/tpl/
+define('CACHE_PATH', INC_PATH . 'cache/'); // /var/www/messages/inc/cache
+define('JSON_PATH', INC_PATH . 'json/'); // /var/www/messages/inc/json/
+define('HIPAA_PATH', '/var/www/HIPAA/');
+define('XSB_PATH', '/var/www/XSB/bin/xsb');
 
-
-define('SITE_PATH', dirname($_SERVER['SCRIPT_NAME']) . '/');
-define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST'] .  SITE_PATH);
-define('JS_URL' ,  SITE_PATH . 'inc/js/');
-define('CSS_URL' , SITE_PATH . 'inc/tpl/');
-//define('JSON_URL', SITE_PATH . 'json/');
-define('JSON_PATH', INC_PATH . 'json/');
+define('SITE_PATH', dirname($_SERVER['SCRIPT_NAME']) . '/'); // /messages/
+define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST'] .  SITE_PATH); // http://crypto.stanford.edu/messages/
+define('JS_URL' ,  SITE_PATH . 'inc/js/'); // /messages/inc/js/
+define('CSS_URL' , SITE_PATH . 'inc/tpl/'); // /messages/inc/tpl/
 
 define('DB_USER', 'gAASAmain');
 define('DB_PASS', 'hieshoon' );
 define('DB_NAME', 'g_AASA_main' );
 define('DB_HOST', 'mysql-user.stanford.edu');
 
-define('MSG_DB', 'hipaa_message');
+define('POLICY_FILE', 'shh.pl');
+define('CACHE_FILE', 'allposscache.php');
 
+define('MSG_DB', 'hipaa_message');
 
 include_once(EZSQL_PATH . "ez_sql_core.php");
 include_once(EZSQL_PATH . "ez_sql_mysql.php");
 include_once(INC_PATH . 'message.php');
 include_once(INC_PATH . 'prolog.php');
 include_once(INC_PATH . 'functions.php');
-$hmsg = new Message();
 
-/* Constants defined in config.php */
+$hmsg = new Message();
 $db = new ezSQL_mysql(DB_USER,DB_PASS,DB_NAME, DB_HOST);
-//$prolog = new Prolog("/var/www/HIPAA", "/var/www/XSB/bin/xsb", "shh.pl");
-$prolog = new Prolog("/afs/ir.stanford.edu/users/s/t/stevetan/cgi-bin/hipaa/HIPAA/", "/afs/ir.stanford.edu/users/s/t/stevetan/cgi-bin/hipaa/XSB/bin/xsb", "shh.pl");
+$prolog = new Prolog(HIPAA_PATH, XSB_PATH, POLICY_FILE);
+//$prolog = new Prolog("/afs/ir.stanford.edu/users/s/t/stevetan/cgi-bin/hipaa/HIPAA/", "/afs/ir.stanford.edu/users/s/t/stevetan/cgi-bin/hipaa/XSB/bin/xsb", "shh.pl");
 ?>
