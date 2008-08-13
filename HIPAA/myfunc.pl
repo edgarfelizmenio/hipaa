6227876c2a2270bea383abf9246426640aedb50e
stevehuynguyen <stevehuynguyen@5d3edfe5-8150-0410-8bef-d1020291cf22>
2008-08-13 02:19:08 +0000

    Added alternate filter_list predicate

--- HIPAA/myfunc.pl
+++ HIPAA/myfunc.pl
@@ -1,18 +1,28 @@
 %:- ['odbc.pl'].
 
 
-% filter_role_list(Unfiltered, Filtered)
-%  
+% filter_list_any(Unfiltered, Filtered)
+%  filters and replaces _h2345 type vars with anything
+%  and then terminates recursion
+filter_list_any([], []).
+filter_list_any([H|T], [X|Y]) :-
+	atom(H),
+	H=X,
+	filter_list(T,Y).
+filter_list_any(_, [X|Y]) :-
+        X='anything',
+	Y = [].
+
+
+% filter_list(Unfiltered, Filtered)
+%  filters the list, skipping over _h234 type variables
 filter_list([], []).
 filter_list([H|T], [X|Y]) :-
 	atom(H),
 	H=X,
 	filter_list(T,Y).
-filter_list(_, [X|Y]) :-
-        X='anything',
-	Y = [].
-
-
+filter_list([_|T], L) :-
+	filter_list(T, L).
 
 
 
