458180a1155cb95160297706b9ae2a5fd38965ff
sharada.sundaram <sharada.sundaram@5d3edfe5-8150-0410-8bef-d1020291cf22>
2010-11-23 22:59:55 +0000

    updating

--- formalization/debug.pl
+++
@@ -1,6 +0,0 @@
-%Remove writeln when we don't want to see messages.  Set writeln to an AND of true or fail.  
-%True if we want to assume unimplemented portions of code pass.
-%Fail if we want to assume unimplemented portions of code always fail.
-debug(X) :-
-  fail.
-  %writeln(X).
