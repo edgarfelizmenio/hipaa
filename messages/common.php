057826871172a559769b5b407e6c14bcfc9e09ab
sharada.sundaram <sharada.sundaram@5d3edfe5-8150-0410-8bef-d1020291cf22>
2010-11-30 03:19:31 +0000

    changed

--- messages/common.php
+++ messages/common.php
@@ -1,11 +1,12 @@
 <?php
 define('ROOTPATH', dirname(__FILE__) . '/'); // /var/www/messages/
+define('ROOTDIR', '/Users/shas/svn/googlecode/'); // /var/www/
 define('INC_PATH', ROOTPATH . 'inc/'); // /var/www/messages/inc/
 define('EZSQL_PATH', INC_PATH . 'ezsql/'); // /var/www/messages/inc/ezsql/
 define('TPL_PATH' , INC_PATH . 'tpl/'); // /var/www/messages/inc/tpl/
 define('CACHE_PATH', INC_PATH . 'cache/'); // /var/www/messages/inc/cache
 define('JSON_PATH', INC_PATH . 'json/'); // /var/www/messages/inc/json/
-define('HIPAA_PATH', ROOTPATH . '../formalization/'); // path to HIPAA/policy files
+define('HIPAA_PATH', ROOTDIR . 'formalization/'); // path to HIPAA/policy files
 
 
 define('SITE_PATH', dirname($_SERVER['SCRIPT_NAME']) . '/'); // /messages/
@@ -13,14 +14,14 @@ define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST'] .  SITE_PATH); // http://cr
 define('JS_URL' ,  SITE_PATH . 'inc/js/'); // /messages/inc/js/
 define('CSS_URL' , SITE_PATH . 'inc/tpl/'); // /messages/inc/tpl/
 
-define('DB_USER', 'user');
-define('DB_PASS', 'user' );
-define('DB_NAME', 'test' );
-define('DB_HOST', 'localhost');
+define('DB_USER', 'gAASAmain');
+define('DB_PASS', 'suhoqjjd' );
+define('DB_NAME', 'g_AASA_main' );
+define('DB_HOST', 'mysql-user.stanford.edu');
 
 define('POLICY_FILE', 'shh.pl');
 define('CACHE_FILE', 'allposscache.php');
-define('XSB_BIN', ROOTPATH . '../XSB/bin/xsb'); // XSB binary file
+define('XSB_BIN', ROOTDIR . 'XSB/bin/xsb'); // XSB binary file
 
 define('MSG_DB', 'hipaa_message');
 
@@ -35,3 +36,4 @@ $db = new ezSQL_mysql(DB_USER,DB_PASS,DB_NAME, DB_HOST);
 $prolog = new Prolog(HIPAA_PATH, XSB_BIN, POLICY_FILE);
 
 ?>
+
--- messages/inc/tpl/msg_menu.php
+++ messages/inc/tpl/msg_menu.php
@@ -4,7 +4,6 @@
     <li><a href="inbox.php">Inbox</a></li>
     <li><a href="write.php">Compose Message</a></li>
     <li><a href="viewmsg.php">Debug (See all messages)</a></li>
-    <li><a href="debugwrite.php">Debug Compose</a></li>
   </ul>
 </div>
 
